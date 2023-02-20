4249bc61d3388c697f7603b27f55079ef5eff884
avagce <avagce@gmail.com>
2023-02-20 18:02:31 +0900

    test commit

---
+++ safe/src/avagce/introduce.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function intro() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        
+        <img id="img_w201903215c939055d152e" src="https://cdn.imweb.me/thumbnail/20190321/5c93905d0d047.jpg" alt="" />
+        
+          Learn React
+       
+      </header>
+    </div>
+  );
+}
+
+export default intro;
