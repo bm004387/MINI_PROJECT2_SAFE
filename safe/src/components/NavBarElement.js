cbd7593d926d91b01289ace63808e22b0d046a20
bm004387 <bm004387@gmail.com>
2023-02-22 13:11:20 +0900

    header update

--- safe/src/components/NavBarElement.js
+++ safe/src/components/NavBarElement.js
@@ -3,34 +3,32 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
-import styles from "../test.css"
+import styles from "../header.css";
 
 function NavBarElement() {
   return (
     <>
     <div className='loginForm'>
       <div>
-      <img src=''/>
+      <img/>
       </div>
       <div>
-        <a>로그인</a> | 
+        <a href='#'>로그인</a> | 
         <a>회원가입</a>
       </div>
     </div>
     <Navbar bg="light" expand="lg">
       <Container className={styles.container}>
-        <Navbar.Brand href="/" className="logo">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/" className="logo"><img src='../images/logo.png'/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/About">About</Nav.Link>
             <Nav.Link href="/Projects">Project</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title="게시판" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">게시글 작성하기</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">게시글 보기</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">게시글 삭제</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
