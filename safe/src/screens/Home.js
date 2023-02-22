28cb79391ec7ff8b06342a2e4510e829380a9ee3
bm004387 <bm004387@gmail.com>
2023-02-22 19:46:23 +0900

    HomePage job add

--- safe/src/screens/Home.js
+++ safe/src/screens/Home.js
@@ -7,10 +7,44 @@ const Home = () => {
     <>
     <div className='main'>
       <Container>
+        <div className='test'>
         <div className='safeList'>
           <ul>
             <li><h4>나에게 필요한 안전교육은?</h4></li>
           </ul>
+        <div className='jop'>
+          <dl>
+            <dt>사업주</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>근로자</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>관리감독자</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>직무교육 대상자</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>특수형태 근로자</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>학생</dt>
+            <dd></dd>
+          </dl>
+        </div>
+        </div>
+        <div className='rightcont'>
+          <ul>
+            <li>fds</li>
+            <li>fds</li>
+          </ul>
+        </div>
         </div>
       </Container>
     </div>
