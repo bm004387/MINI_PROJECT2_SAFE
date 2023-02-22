4ea6e987b3b7759b50bc799bfbaa8a88fa9b28db
avagce <avagce@gmail.com>
2023-02-23 01:36:09 +0900

    add login and source version V6

--- safe/src/App.js
+++ safe/src/App.js
@@ -10,6 +10,14 @@ import NavBarElement from './components/NavBarElement';
 import Intro from './avagce/introduce';
 import Movie from './avagce/movie';
 
+import LoginPage from "./pages/Login"; // 로그인 페이지 Import
+import Homemanage from "./pages/Home"; // 로그인 후 회원정보를 관리하는 페이지 Import
+import Register from "./pages/Register"; // 회원가입 페이지 Import
+import Forgot from "./pages/Forgot"; //아이디/비밀번호 찾기 페이지 import
+import Callback from "./pages/Callback"; //네이버로그인 콜백페이지
+import Address from "./pages/Address"; // 주소검색 페이지
+import Ipopup from "./pages/Ipopup"; // 주소검색 페이지
+import Mpopup from "./pages/Mpopup"; // 주소검색 페이지
 
 
 function App() {
@@ -23,6 +31,17 @@ function App() {
         <Route path="/Projects" element={<Projects/>}/>
         <Route path="/Login" element={<Login/>}/>
         <Route path="/SighUp" element={<SighUp/>}/>
+
+        <Route exact path="/LoginPage" element={<LoginPage/>} />
+      <Route exact path="/management" element={<Homemanage/>} />
+      <Route exact path="/management-callback" element={<Callback/>} />
+      <Route exact path="/register" element={<Register/>} />
+      
+      <Route exact path="/update" element={<Register info={true} />} />
+      <Route exact path="/forgot" element={<Forgot/>} />
+      <Route exact path="/address" element={<Address/>} />
+      <Route exact path="/ipopup" element={<Ipopup/>} />
+      <Route exact path="/mpopup" element={<Mpopup/>} />
       </Routes>
     </Router>
   );
--- safe/src/index.js
+++ safe/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
+import * as serviceWorker from "./serviceWorker.js";
 
 
 import Intro from './avagce/introduce';
