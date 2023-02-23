7520c8017f5863a4abe96de0ae5c2111c88659e9
bm004387 <bm004387@gmail.com>
2023-02-23 18:07:59 +0900

    Home batch success

--- safe/src/screens/Home.js
+++ safe/src/screens/Home.js
@@ -6,48 +6,50 @@ const Home = () => {
   return (
     <>
     <div className='main'>
+        <div className='maintop'>
       <Container>
-        <div className='test'>
-          <div className='topcont'>
-            <ul>
-              <li><h4>나에게 필요한 안전교육은?</h4></li>
-              <li><span>아래 교육유형을 선택하시면 필요하신 교육과정</span>을 확인하실 수 있습니다</li>
-            </ul>
-          <div className='jop'>
-            <dl>
-              <dt>사업주</dt>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>근로자</dt>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>관리감독자</dt>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>직무교육 대상자</dt>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>특수형태 근로자</dt>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>학생</dt>
-              <dd></dd>
-            </dl>
+          <div className='test'>
+            <div className='topcont'>
+              <ul>
+                <li><h4>나에게 필요한 안전교육은?</h4></li>
+                <li><span>아래 교육유형을 선택하시면 필요하신 교육과정</span>을 확인하실 수 있습니다</li>
+              </ul>
+            <div className='jop'>
+              <dl>
+                <dt>사업주</dt>
+                <dd></dd>
+              </dl>
+              <dl>
+                <dt>근로자</dt>
+                <dd></dd>
+              </dl>
+              <dl>
+                <dt>관리감독자</dt>
+                <dd></dd>
+              </dl>
+              <dl>
+                <dt>직무교육 대상자</dt>
+                <dd></dd>
+              </dl>
+              <dl>
+                <dt>특수형태 근로자</dt>
+                <dd></dd>
+              </dl>
+              <dl>
+                <dt>학생</dt>
+                <dd></dd>
+              </dl>
+            </div>
+            </div>
+            <div className='rightcont'>
+              <ul>
+                <li><a href='#'>개별사업장<br></br> 안전보건교육</a></li>
+                <li><a href='#'>건설업 기초안전<br></br> 보건교육</a></li>
+              </ul>
+            </div>
           </div>
-          </div>
-          <div className='rightcont'>
-            <ul>
-              <li>fds</li>
-              <li>fds</li>
-            </ul>
-          </div>
-        </div>
       </Container>
+        </div>
       <Container>
         <div className='ftcont'>
           <div className='ftcont1'>
@@ -66,8 +68,30 @@ const Home = () => {
           <div className='ftcont2'>
             <div className='ntcfaq'>
               <ul>
-                <li><a>공지사항</a></li>
-                <li><a>자주묻는질문들</a></li>
+                <li><a href='a'>공지사항</a>
+                  <div className='notice'>
+                    <ul>
+                      <li>2024년 안전보건교육기관 평가지표 공표<span className='date'>2023-02-23</span></li>
+                      <li>건설업 기초안전보건교육 안내문 게시<span className='date'>2023-02-23</span></li>
+                      <li>'24년 기관평가 지표 개편(안) 공지<span className='date'>2023-02-23</span></li>
+                      <li>2022년 안전보건교육 안내서 안내<span className='date'>2023-02-23</span></li>
+                      <li>"중대재해 감축 로드맵" 수립을 위한 토론회 안내<span className='date'>2023-02-23</span></li>
+                      <li>건설업 기초안전보건교육 이수자 개인정보 정정 절차 안내<span className='date'>2023-02-23</span></li>
+                    </ul>
+                  </div>
+                </li>
+                <li><a href='a'>자주묻는질문들</a>
+                  <div className='notice'>
+                    <ul>
+                      <li>2024년 안전보건교육기관 평가지표 공표<span className='date'>2023-02-23</span></li>
+                      <li>건설업 기초안전보건교육 안내문 게시<span className='date'>2023-02-23</span></li>
+                      <li>'24년 기관평가 지표 개편(안) 공지<span className='date'>2023-02-23</span></li>
+                      <li>2022년 안전보건교육 안내서 안내<span className='date'>2023-02-23</span></li>
+                      <li>"중대재해 감축 로드맵" 수립을 위한 토론회 안내<span className='date'>2023-02-23</span></li>
+                      <li>건설업 기초안전보건교육 이수자 개인정보 정정 절차 안내<span className='date'>2023-02-23</span></li>
+                    </ul>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
