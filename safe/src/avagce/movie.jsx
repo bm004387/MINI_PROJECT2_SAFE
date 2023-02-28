c25199793c21e6bada6cc72b9ad306188fcfce1b
bm004387 <bm004387@gmail.com>
2023-02-28 10:37:37 +0900

    quake, fire add

--- safe/src/avagce/movie.jsx
+++ safe/src/avagce/movie.jsx
@@ -9,38 +9,79 @@ import './movie.css'
 function Movie() {
   return (
     <>
-      <div className="d-flex justify-content-around movie_list">
-        <Card style={{ width: '15rem' }}>
-          <Card.Img variant="top" src={movie1} />
-          <Card.Body>
-            <Card.Title>지진이 발생했을때는 어떻게 해야할까?</Card.Title>
-            <Button variant="primary" href='/movie1'>교육 영상 시청하기</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '15rem' }}>
-          <Card.Img variant="top" src={movie2}/>
-          <Card.Body>
-            <Card.Title>지진대피법</Card.Title>
-            <Button variant="primary" href='/movie2'>교육 영상 시청하기</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '15rem' }}>
-          <Card.Img variant="top" src={movie3}/>
-          <Card.Body>
-            <Card.Title>지진 국민행동요령</Card.Title>
-            <Card.Text>
-              2TV 생생정보 - 생생정보통 지진 안전지대 아니다
-            </Card.Text>
-            <Button variant="primary" href='/movie3'>교육 영상 시청하기</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '15rem' }}>
-          <Card.Img variant="top" src={movie4}/>
-          <Card.Body>
-            <Card.Title>지진 발생 시 어떡해야 할까?</Card.Title>
-            <Button variant="primary" href='/movie4'>교육 영상 시청하기</Button>
-          </Card.Body>
-        </Card>
+      <div className='safedu'>
+        <div className='quake'>
+          <h2>지진 발생 시 안전교육</h2>
+          <div className="d-flex justify-content-around movie_list">
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie1} />
+              <Card.Body>
+                <Card.Title>지진이 발생했을때는 어떻게 해야할까?</Card.Title>
+                <Button variant="primary" href='/movie1'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie2}/>
+              <Card.Body>
+                <Card.Title>지진대피법</Card.Title>
+                <Button variant="primary" href='/movie2'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie3}/>
+              <Card.Body>
+                <Card.Title>지진 국민행동요령</Card.Title>
+                <Card.Text>
+                  2TV 생생정보 - 생생정보통 지진 안전지대 아니다
+                </Card.Text>
+                <Button variant="primary" href='/movie3'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie4}/>
+              <Card.Body>
+                <Card.Title>지진 발생 시 어떡해야 할까?</Card.Title>
+                <Button variant="primary" href='/movie4'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+        <div className='fire'>
+          <h2>화재 발생 시 안전교육</h2>
+          <div className="d-flex justify-content-around movie_list">
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie1} />
+              <Card.Body>
+                <Card.Title>지진이 발생했을때는 어떻게 해야할까?</Card.Title>
+                <Button variant="primary" href='/movie1'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie2}/>
+              <Card.Body>
+                <Card.Title>지진대피법</Card.Title>
+                <Button variant="primary" href='/movie2'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie3}/>
+              <Card.Body>
+                <Card.Title>지진 국민행동요령</Card.Title>
+                <Card.Text>
+                  2TV 생생정보 - 생생정보통 지진 안전지대 아니다
+                </Card.Text>
+                <Button variant="primary" href='/movie3'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src={movie4}/>
+              <Card.Body>
+                <Card.Title>지진 발생 시 어떡해야 할까?</Card.Title>
+                <Button variant="primary" href='/movie4'>교육 영상 시청하기</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
       </div>
     </>
   );
