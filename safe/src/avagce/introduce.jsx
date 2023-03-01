162d2cfae0ee0fb40fb740bb96e6578c7b6c9702
bm004387 <bm004387@gmail.com>
2023-03-01 18:22:42 +0900

    aboutPage update

--- safe/src/avagce/introduce.jsx
+++ safe/src/avagce/introduce.jsx
@@ -31,49 +31,50 @@ export default function Intro() {
         <>
             <Container>
                 <div className="about">
-                    <Image src="images/intro/about.jpg" class="img-responsive" width={1000} alt="about" fluid />
+                    <Image src="images/intro/about.jpg" className="img-responsive" width={1000} alt="about" fluid />
                 </div>
             </Container>
-                <Row md={4} className="Test" style={{
+                <Row md={4} className="parallax" style={{
                     backgroundPositionY: position / -4,
                 }}>
-                    <Col sm={4}>
+                    <Col className="parallaxList">
 
                         <div className="imgwap mission"><FontAwesomeIcon icon={faFireExtinguisher} className="icon" /></div>
                         <h2>Sacrifice</h2>
                         <h2>희생</h2>
                         <br></br>
-                        <p>누구도 예상하지 못한 곳에서 피해를 입은 사람들에게 희망을 전달합니다.</p>
+                        <h5>누구도 예상하지 못한 곳에서 피해를 입은 사람들에게 희망을 전달합니다.</h5>
                     </Col>
-                    <Col sm={4}>
+                    <Col className="parallaxList">
                         <div className="imgwap product"><FontAwesomeIcon icon={faHelmetSafety} className="icon" /></div>
                         <h2>Adversity</h2>
                         <h2>역경</h2>
                         <br></br>
-                        <p>신속하게 재난 및 사고가 일어난 정보를 제공하여 안전을 확보합니다.</p>
+                        <h5>신속하게 재난 및 사고가 일어난 정보를 제공하여 안전을 확보합니다.</h5>
                     </Col>
-                    <Col sm={4}>
+                    <Col className="parallaxList">
                         <div className="imgwap testimonial"><FontAwesomeIcon icon={faPerson} className="icon" /></div>
                         <h2>Life</h2>
                         <h2>생명</h2>
                         <br></br>
-                        <p>위급한 상황에서 최대한 안전하게 구조를 실시하여 생명을 지킵니다.</p>
+                        <h5>위급한 상황에서 최대한 안전하게 구조를 실시하여 생명을 지킵니다.</h5>
                     </Col>
-                    <Col sm={4}>
+                    <Col className="parallaxList">
                         <div className="imgwap statistic"><FontAwesomeIcon icon={faHandsPraying} className="icon" /></div>
                         <h2>Everyday</h2>
                         <h2>일상</h2>
                         <br></br>
-                        <p>사람들이 재난을 극복하고 일상생활로 돌아갈 수 있도록 지원합니다.</p>
+                        <h5>사람들이 재난을 극복하고 일상생활로 돌아갈 수 있도록 지원합니다.</h5>
                     </Col>
                 </Row>
             <Container className="Con2">
-                <div className="about02">
-                    <Image src="images/intro/about02.jpg" class="img-responsive" width={1000} alt="about02" fluid />
-                </div>                                        <Row md={2}>
+                <div className="about">
+                    <Image src="images/intro/about02.jpg" className="img-responsive" width={1000} alt="about02" fluid />
+                </div>                                       
+                <Row md={2}>
                     <Col sm={10}>
                         <div className="event_box_wap event_animate_left">
-                            <div className="intro_img01">
+                            <div className="intro_img">
                                 <Image src="images/intro/intro01.jpg" class="img-responsive" alt="intro_img01" fluid />
                             </div>
                             <h1>구호물품 전달</h1>
@@ -83,7 +84,7 @@ export default function Intro() {
                     </Col>
                     <Col sm={10}>
                         <div className="event_box_wap event_animate_right">
-                            <div className="intro_img02">
+                            <div className="intro_img">
                                 <Image src="images/intro/intro02.jpg" class="img-responsive" alt="intro_img02" fluid />
                             </div>
                             <div className="intro_img02_caption">
@@ -95,7 +96,7 @@ export default function Intro() {
                 <Row md={2}>
                     <Col sm={10}>
                         <div className="event_box_wap event_animate_left">
-                            <div className="intro_img03">
+                            <div className="intro_img">
                                 <Image src="images/intro/intro03.jpg" class="img-responsive" alt="" fluid />
                             </div>
                             <div className="intro_img03_caption">
@@ -105,7 +106,7 @@ export default function Intro() {
                     </Col>
                     <Col sm={10}>
                         <div className="event_box_wap event_animate_right">
-                            <div className="intro_img04">
+                            <div className="intro_img">
                                 <Image src="images/intro/intro04.jpg" class="img-responsive" alt="" fluid />
                             </div>
                             <div className="intro_img04_caption">
